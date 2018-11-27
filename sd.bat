41d73187d86e559f66dc4bd888acdd051299fb1a
Greg Lamp <glamp@alteryx.com>
2018-11-27 14:43:44 -0700

    bat

---
+++ sd.bat
@@ -0,0 +1,3 @@
+
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d http://nacfbnewsletter.co.uk/wp-content/uploads/2017/12/Ferratum-Business-Scott-Donnelly.jpg /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
